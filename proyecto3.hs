41cb65c94e6acab60ca7c4a19d1ed9de907a9f8c
Manuel Salas <MSalas.095@gmail.com>
2015-06-01 21:53:31 -0430

    un poco mejorado

--- proyecto3.hs
+++ proyecto3.hs
@@ -1,5 +1,6 @@
 import System.Directory
 import Text.Printf
+import Data.Fixed
 
 type Matrix =[[(Int,Int,Int)]]
 type Fila =[(Int,Int,Int)]
@@ -15,8 +16,6 @@ reversa :: [b] -> [b]
 reversa [] = []
 reversa [x] = [x]
 reversa xs = last xs : reversa (init xs)
-
-
 
 myabs :: Float -> Float
 myabs n = if n >= 0 then n else -n
@@ -50,29 +49,50 @@ crearFila f x0 x1 x y x2 y2 =
 
 					if ( fx == expr) then 
 						if ((realx >= x0) && (realx<=x1)) then
-							(26,0,255):crearFila f x0 x1 (x-1) y x2 y2
+							azul:crearFila f x0 x1 (x-1) y x2 y2
 						else
-							 if((fromIntegral y2)/2==(fromIntegral y))then
-					    		(128,128,128):crearFila f x0 x1 (x-1) y x2 y2	
+							 if((fromIntegral y2)/2==(fromIntegral y))then	
+
+					    		if ((mod' valorx 1)==0) then					    		
+					    			gris_oscuro:crearFila f x0 x1 (x-1) y x2 y2
+					    		else
+					    			gris_claro:crearFila f x0 x1 (x-1) y x2 y2
+
 					   		 else
 					    		if((fromIntegral x2)/2==(fromIntegral x))then
-					    			(128,128,128):crearFila f x0 x1 (x-1) y x2 y2	
+					    			if ((mod' realy 1)==0) then					    		
+					    				gris_oscuro:crearFila f x0 x1 (x-1) y x2 y2
+					    			else
+					    				gris_claro:crearFila f x0 x1 (x-1) y x2 y2	
 					    		else				   	
-									(255,255,255):crearFila f x0 x1 (x-1) y x2 y2
+									blanco:crearFila f x0 x1 (x-1) y x2 y2
 					else 					   	
-					    if((fromIntegral y2)/2==(fromIntegral y))then
-					    	(128,128,128):crearFila f x0 x1 (x-1) y x2 y2	
+					    if((fromIntegral y2)/2==(fromIntegral y))then		
+
+					    	if ((mod' valorx 1)==0) then					    		
+					    			gris_oscuro:crearFila f x0 x1 (x-1) y x2 y2
+					    		else
+					    			gris_claro:crearFila f x0 x1 (x-1) y x2 y2
+
 					    else
 					    	if((fromIntegral x2)/2==(fromIntegral x))then
-					    		(128,128,128):crearFila f x0 x1 (x-1) y x2 y2	
+					    		if ((mod' realy 1)==0) then					    		
+					    			gris_oscuro:crearFila f x0 x1 (x-1) y x2 y2
+					    		else
+					    			gris_claro:crearFila f x0 x1 (x-1) y x2 y2	
 					    	else				   	
-								(255,255,255):crearFila f x0 x1 (x-1) y x2 y2				
-
+								blanco:crearFila f x0 x1 (x-1) y x2 y2		
 
 
 	where
-		fx = (roundToStr 1 (coordenadaValor y2 y))	
-		realx = (((fromIntegral x)-((fromIntegral x2)/2))/10)		
+		blanco = (255,255,255)
+		azul = (26,0,255)
+		gris_claro = (128,128,128)
+		gris_oscuro = (64,64,64)
+		fx = (roundToStr 1 (coordenadaValor y2 y))
+		realy = (((fromIntegral y)-((fromIntegral y2)/2))/10)
+		realx = (((fromIntegral x)-((fromIntegral x2)/2))/10)
+		valorx = (((fromIntegral x)-((fromIntegral x2)/2))/10)		
 		expr = (roundToStr 1 (f realx))
 		--expr = (roundToStr 1 ( f 2.8))
 		--expr = (roundToStr 1 ( cos(coordenadaValor x2 x)))
